404d32dd03cac05fba70b5f7cc5ebe5ead735ec3
SpanishTorture <lovellelightlakefield@gmail.com>
2018-06-26 18:09:43 -0400

    First commit

--- Sort 3.py
+++ Sort 3.py
@@ -1,27 +1,31 @@
 def Sort(Num):
-    Solution = [None]*(max(Num)+1)
-
-    #Converts the number to a string and assigns string to Solution
+    Count = [0]*(max(Num)+1)
+    Solution = list()
+    #Counts how many of each number there are in Num
     for n in range(len(Num)):
-        if Solution[Num[n]]==None:
-            Solution[Num[n]]=str(Num[n])
+        Count[Num[n]]+=1
 
-        #handles duplicate numbers
-        else:
-            Solution[Num[n]]=str(Solution[Num[n]]) + " " + str(Num[n])
+    #Creates ordered list
+    for n in range(len(Count)):
+        while Count[n]>0:
+            Solution.append(n)
+            Count[n]-=1
+    print(Solution)
+    return(Solution)
 
-    #prints solution
-    for n in range(len(Solution)):
-        if Solution[n] != None:
-            print (Solution[n], end=" ")
-    print("")
-
+def Check(Test):
+    Test = Sort(Test)
+    for n in range(1,len(Test)):
+        if Test[n]<Test[n-1]:
+            print ("Fail")
+            return False
+    print("Sucess")
 
 #Tests
 Test1=[3, 7, 2, 8, 11, 15, 2]
 Test2=[3,47,22,81,113,54,2,81,78,1,77,222,7478,21]
 Test3=[3057, 2, 614531, 6275, 1549, 5523, 966463, 5751, 3200, 22474, 2, 9307, 7899, 180, 4937, 5849, 92288, 38610, 42693, 85181, 53432, 33234, 65011, 55615, 84925, 37617, 15353, 11042, 9, 11523, 21914, 91634, 38014, 65294, 85786, 98407, 17040, 52998, 46685, 34322]
 
-Sort(Test1)
-Sort(Test2)
-Sort(Test3)
+Check(Test1)
+Check(Test2)
+Check(Test3)
